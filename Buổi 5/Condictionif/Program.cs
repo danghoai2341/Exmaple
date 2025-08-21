a00016bf0d91e353b1242f783680d26b016d47c7
danghoai2341 <hoaid111@gmail.com>
2025-08-21 11:10:15 +0700

    add
    add

---
+++ Buổi 5/Condictionif/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 5;
+        if (a == 10)
+        {
+            Console.WriteLine("Gia tri bien a la 10");
+        }
+        else
+        {
+            Console.WriteLine("Gia tri bien a khong phai la 10");
+        }
+    }
+}
