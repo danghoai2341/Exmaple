0c88e3626ee5b034f3dbf59936c6764e3197693e
danghoai2341 <hoaid111@gmail.com>
2025-08-21 10:14:21 +0700

    add
    add

---
+++ Buổi 4/Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DateTime dateTime = DateTime.Now;
+        Console.WriteLine($"{dateTime.Hour}{dateTime.Minute}{dateTime.Second}");
+        
+    }
+}
---
+++ Buổi 4/ExampleOpreator/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.WriteLine("Hay nhap gia tri cho chieu rong:");
+        width = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Hay nhap gia tri cho chieu cao:");
+        height = Convert.ToInt32(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine("Dien tich hinh chu nhat la: " + area);
+    }
+}
