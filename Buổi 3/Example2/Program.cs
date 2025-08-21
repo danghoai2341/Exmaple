cc5f2b5da9f403d4fcef7ece79de775b87c26959
danghoai2341 <hoaid111@gmail.com>
2025-08-21 09:50:21 +0700

    add
    add

---
+++ Buổi 3/Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿//tinh dien tich hinh tron khi biet ban kinh
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap ban kinh hinh tron: ");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double s = Math.PI * r * r;
+        Console.WriteLine("Dien tich hinh tron la: " + s);
+    }
+}
+        
---
+++ Buổi 3/Example3/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("nhap ho ten: ");
+        string hoTen = Console.ReadLine();
+        Console.WriteLine("nhap tuoi: ");
+        int tuoi = Convert.ToInt32(Console.ReadLine());
+        //
+        Console.WriteLine($"Ho ten: {hoTen}, Tuoi:{tuoi} ");
+    }
+}
