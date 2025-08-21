ccba0a6e03a9bf2b0786f6b643091461c9292f3d
danghoai2341 <hoaid111@gmail.com>
2025-08-21 09:29:20 +0700

    add
    add

---
+++ Buổi 2/example2/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so nguyen thu nhat: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap so nguyen thu hai: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int tong = a + b;
+        Console.WriteLine("Tong hai so la: " + tong);
+    }
+}
