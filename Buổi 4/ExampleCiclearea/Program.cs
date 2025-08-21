2ce698ac43c8eae713682ce95f737bcf84b06838
danghoai2341 <hoaid111@gmail.com>
2025-08-21 10:21:36 +0700

    add
    add

---
+++ Buổi 4/ExampleCiclearea/Program.cs
@@ -0,0 +1,14 @@
+﻿//nhap ban kinh hinh tron va tinh dien tich chu vi
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Nhap ban kinh hinh tron: ");
+        Double r = Convert.ToDouble(Console.ReadLine());
+        Double area = Math.PI * r * r;
+        Double pre = 2 * Math.PI * r;
+        Console.WriteLine($"Chu vi la: {pre}");
+        Console.WriteLine($"Dien tich la: {area}");
+    }
+}
