73ea76ac3da5a3b42e3860480b8ff4262768d4a3
danghoai2341 <hoaid111@gmail.com>
2025-08-21 09:20:52 +0700

    add
    add

--- Buổi 2/example/Program.cs
+++ Buổi 2/example/Program.cs
@@ -1,7 +1,10 @@
-﻿public class Program
+﻿//pn ra man hinh yeu cau nhap mot ky tu ban phim, sau do nhap mot so roi in ra man hinh
+public class Program
 {
     public static void Main(string[] args)
     {
-        
+        Console.Write("Hay nhap mot so tren ban phim: ");
+        int so = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("" + so);
     }
 }
